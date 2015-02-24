ac78ef49da5c41462ec77807d9e7526c6292590c
Gabrielganchev <gabriel_ganchev@abv.bg>
2015-02-25 00:00:33 +0200

    still crap
    black bla

--- 1-Fill-tetrahedron-with-water.py
+++ 1-Fill-tetrahedron-with-water.py
@@ -8,7 +8,7 @@ def fill_tetrahedron():
     a=int(input("number for a"))#fancy
 
     volume =((a**3)/(6*(math.sqrt(2)))/1000)
-    volume=round(volume, 2)#week sorcery
+    volume=round(volume, 2)#weak sorcery
     print(volume,"liters of water")
     return 
 fill_tetrahedron()
--- disappointment(doesntwork).py
+++ disappointment(doesntwork).py
@@ -1,21 +1,21 @@
 import math
 
-water = int(input("water:"))  #Enter the amount of water in litres
-n = input("Enter the number of tetrahedrons:")
+water = int(input("water:"))
+num = int(input(" num for tetrahedrons:"))
 tetrahedrons = []
-for i in range(int(n)):
-    edge = input("Enter the edge of a tetrahedron:")
-    tetrahedrons.append(int(edge))
+for i in range(int(num)):
+    edge = int(input("edges for tetrahedron:"))
+    tetrahedrons.append(edge)
     tetrahedrons.sort()
 
 
 
 def fill_tetrahedron(num):
-    volume = (int(num) ** 3) // (6.0 * (2 ** 0.5))
-    volume_in_l = volume / 1000
+    volume =((a**3)/(6*(math.sqrt(2)))/1000)
+    volume=round(volume, 2)
 
-def tetrahedron_filled(tetrahedrons, water):
-    if water >= 0:
+def tetrahedron_filled(tetrahedrons, water):#water reads it like inside variable // :(
+    if water >= c:
         for number in tetrahedrons:
             value = fill_tetrahedron(number)
             water = water - value
@@ -23,4 +23,7 @@ def tetrahedron_filled(tetrahedrons, water):
             full_tetrahedrons += 1
         return full_tetrahedrons
 
-print (tetrahedron_filled(tetrahedrons, water))
+print(tetrahedron_filled(water ,tetrahedrons))
+print(tetrahedron_filled())
+
+
