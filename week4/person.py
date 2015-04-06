1654c48968a873c0aaff8edbb6b19e758235616f
Gabrielganchev <gabriel_ganchev@abv.bg>
2015-04-06 23:35:03 +0300

    new shit
    new

---
+++ week4/person.py
@@ -0,0 +1,14 @@
+first= input("Write your first name please")
+second= input("Write your second name please")
+last= input("Write your last name please")
+birth = int(input("Your Birth date please "))
+
+log = {
+    "name"=first,
+    second_name=second,
+    last_name=last,
+    "age"=birth
+
+       }
+print(log)
+
