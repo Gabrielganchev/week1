8c95357c75d0c3bfb9ebd670fcac6fef83326c6a
Gabrielganchev <gabriel_ganchev@abv.bg>
2015-04-22 19:28:56 +0300

    another test practise

---
+++ test function.py
@@ -0,0 +1,12 @@
+# testing function  ( practice)   / simple
+
+
+
+def shout(phrase):
+    if phrase == phrase.upper()or phrase == phrase.isstr():
+        print("You are  shouting please stop")
+        return ("Upper phrase  <-_-> ")
+    else:
+        print ("why are you  not shouting")
+        return  (".lower() the user isnt shouting")
+# shout(input("what the fuck is going on") #input returns str() always will work as phrase
