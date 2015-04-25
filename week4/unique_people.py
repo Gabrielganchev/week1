7a7e87db20aa4c45ec55c15513cc3d01f1821543
Gabrielganchev <gabriel_ganchev@abv.bg>
2015-04-25 23:38:53 +0300

    new unique_people

---
+++ week4/unique_people.py
@@ -0,0 +1,10 @@
+def get_people_count(activity):
+    counted_people = []
+    
+    for person in activity:
+        if person not in counted_people:
+            counted_people += [person]
+
+    return len(counted_people)
+
+print(get_people_count(["Rado", "Ivo", "Maria", "Anneta", "Rado", "Rado", "Anneta", "Ivo", "Maria", "Rado"]))
